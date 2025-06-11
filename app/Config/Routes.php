ce843f9d3821eb136ed8924ba4ce67ebad847e64
pauromaoluche <pauluci125romao.pedro@hotmail.com>
2025-06-11 10:23:12 -0300

    criando rotas e controllers de noticias

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Controllers\NewsController;
 use App\Controllers\Pages;
 use CodeIgniter\Router\RouteCollection;
 
@@ -7,5 +8,9 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+$routes->get('news', [NewsController::class, 'index']);           // Add this line
+$routes->get('news/(:segment)', [NewsController::class, 'show']);
+
 $routes->get('pages', [Pages::class, 'index']);
 $routes->get('(:segment)', [Pages::class, 'view']);
---
+++ app/Controllers/NewsController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use App\Models\NewsModel;
+use CodeIgniter\Exceptions\PageNotFoundException;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class NewsController extends BaseController
+{
+    public function index()
+    {
+        $model = model(NewsModel::class);
+
+        $data = [
+            'news_list' => $model->getNews(),
+            'title'     => 'News archive',
+        ];
+
+        return view('templates/header', $data)
+            . view('news/index')
+            . view('templates/footer');
+    }
+
+    public function show(?string $slug = null)
+    {
+        $model = model(NewsModel::class);
+
+        $data['news'] = $model->getNews($slug);
+
+        if ($data['news'] === null) {
+            throw new PageNotFoundException('Cannot find the news item: ' . $slug);
+        }
+
+        $data['title'] = $data['news']['title'];
+
+        return view('templates/header', $data)
+            . view('news/view')
+            . view('templates/footer');
+    }
+}
